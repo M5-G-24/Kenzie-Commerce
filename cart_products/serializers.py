a63e5e217de04228ad2bd80316b52b1b73e38a8b
Mauí Buarque <maui.buarque1@gmail.com>
2023-07-04 15:39:00 -0300

    feat: cart product views

--- cart_products/serializers.py
+++ cart_products/serializers.py
@@ -6,3 +6,14 @@ class CartProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = CartProduct
         fields = ["id", "cart", "product", "amount"]
+
+
+class CartProductUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CartProduct
+        fields = ["id", "cart", "product", "amount"]
+        extra_kwargs = {
+            "id": {"read_only": True},
+            "cart": {"read_only": True},
+            "product": {"read_only": True},
+        }
