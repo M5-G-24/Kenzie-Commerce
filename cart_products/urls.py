ae057f04479da98b07c96ed50ad80d3a8376fcd3
Christian <christianryuji@hotmail.com>
2023-07-06 14:39:10 -0300

    core: rebuild

--- cart_products/urls.py
+++ cart_products/urls.py
@@ -2,6 +2,14 @@ from django.urls import path
 from .views import CartProductView, CartProductDetailView
 
 urlpatterns = [
-    path('cart/<int:cart_id>/cart-products/', CartProductView.as_view(), name='cart-products'),
-    path('cart/<int:cart_id>/cart-products/<int:pk>/', CartProductDetailView.as_view(), name='cart-product-detail'),
+    path(
+        "cart_products/<int:user_id>/",
+        CartProductView.as_view(),
+        name="cart-products",
+    ),
+    path(
+        "cart_products/<int:user_id>/cart-products/<int:pk>/",
+        CartProductDetailView.as_view(),
+        name="cart-product-detail",
+    ),
 ]
