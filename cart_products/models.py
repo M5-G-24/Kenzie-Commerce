9f86ebe845c22264c7d8bf1d5fac45fb1b8b914a
Mauí Buarque <maui.buarque1@gmail.com>
2023-07-06 11:00:57 -0300

    feat: total cart product

--- cart_products/models.py
+++ cart_products/models.py
@@ -1,7 +1,14 @@
 from django.db import models
+from products.models import Product
+from math import ceil
 
 
 class CartProduct(models.Model):
     cart = models.ForeignKey("carts.Cart", on_delete=models.CASCADE)
     product = models.ForeignKey("products.Product", on_delete=models.CASCADE)
     amount = models.IntegerField()
+    total = models.FloatField()
+
+    def save(self, *args, **kwargs):
+        self.total = ceil(self.product.price * self.amount * 100) / 100
+        super().save(*args, **kwargs)
