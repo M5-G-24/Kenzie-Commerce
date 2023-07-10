40e86137f40f46a793fdedb04c755958286ef670
Gabriel Góes <matt_cariusp@hotmail.com>
2023-07-10 16:54:58 -0300

    feat: Users listing w/ adminOnly

--- users/views.py
+++ users/views.py
@@ -1,8 +1,16 @@
 from rest_framework import generics
 from .models import User
 from .serializers import UserSerializer
+from rest_framework.permissions import IsAdminUser
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 
-class UserListCreateAPIView(generics.ListCreateAPIView):
+class UserCreateAPIView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+class UserListAPIView(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAdminUser]
