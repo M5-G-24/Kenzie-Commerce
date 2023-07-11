ec523228511d5505e1d342c34269546f22c92a20
Mauí Buarque <maui.buarque1@gmail.com>
2023-07-11 14:38:06 -0300

    fix: address detail view

--- addresses/views.py
+++ addresses/views.py
@@ -1,8 +1,6 @@
-from django.shortcuts import get_object_or_404
 from rest_framework import generics
 from .models import Address
 from .serializer import AddressSerializer
-from users.models import User
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from users.permissions import IsOwnerOnly
@@ -31,7 +29,6 @@ class AddressDetailsView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Address.objects.all()
     serializer_class = AddressSerializer
 
-    def perform_create(self, serializer):
-        pk = self.kwargs.get("pk")
-        user = get_object_or_404(User, pk=pk)
-        serializer.save(user=user)
+    def get_object(self):
+        user = self.request.user
+        return self.queryset.get(user=user)
